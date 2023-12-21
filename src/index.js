111a926428c866d8e0a68b97bb34311e891e5810
Shubham <tysmgroups@gmail.com>
2023-12-21 21:23:55 +0530

    1. Class Inheritance Concept

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+class car {
+  constructor(name){
+    this.brand = name;
+  }
+  present(){
+    return 'I have a '+ this.brand;
+  }
+}
+
+class Model extends car{
+  constructor(name,mod){
+    super(name);
+    this.model=mod;
+  }
+  show(){
+    return this.present() + ', it is a '+ this.model
+  }
+}
+
+const mycar = new Model('Ford','Mustang');
+console.log(mycar.show());
